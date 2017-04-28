4b933c1c1747d74a6f11cb04e2fa48e3bd35c706
PlushBeard <bicksloy@gmail.com>
2017-04-28 19:06:50 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  //ogoloshuemo POST zminni
+  $lastName = $_POST['last_name']; //prizvyche
+  $firstName = $_POST['first_name']; //im'ya
+
+  //Yakcho pole "Prizvyche" i "Im'ya" ne puste
+  if (!empty($lastName) && !empty($firstName))
+  {
+    //pyshemo
+    echo "Vitaiu Vas $lastName $firstName. <br />";
+  }
+  else //abo
+  {
+    //pyshemo
+    echo "Yak Vy posmily natysnuty ciu knopky?";
+    //i povertaemoc nazad na index.html
+    echo "<meta http-equiv='refresh' content='15; url=index.html' />";
+  }
+?>
+
